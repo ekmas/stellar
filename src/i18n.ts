8b104c803ce611401a90754e2010d30f40432b58
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-04 18:53:16 +0100

    added i18n.ts

---
+++ src/i18n.ts
@@ -0,0 +1,29 @@
+type LanguageKeys = keyof typeof LANGUAGES
+
+type LanguageObject<T> = {
+  [Lang in LanguageKeys]: T
+}
+
+export type LandingPageObj = {
+  description: string
+  getStartedBtnText: string
+  githubBtnText: string
+}
+
+export const LANGUAGES = {
+  en: 'Engleski',
+  sr: 'Srpski',
+} as const
+
+export const LANDING_PAGE: LanguageObject<LandingPageObj> = {
+  en: {
+    description: 'Modern type-safe documentation theme for Astro.',
+    getStartedBtnText: 'Get started',
+    githubBtnText: 'Source code',
+  },
+  sr: {
+    description: 'Moderna Astro tema za dokumentaciju sa Typescript podrškom.',
+    getStartedBtnText: 'Počni',
+    githubBtnText: 'Izvorni kod',
+  },
+} as const
