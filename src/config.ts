84cb7ba1fb590104ac13904bc6189b940476567a
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-10 22:42:18 +0100

    added github edit url to config

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,10 @@ export const TWITTER_USERNAME = null
 // enter your twitter username if you have it
 export const GITHUB_REPO_URL = 'https://github.com/ekmas/astro-docs'
 
+export const GITHUB_EDIT_URL = GITHUB_REPO_URL + '/edit/main/src/pages/'
+// used for edit url link at the bottom of each page in docs so users can edit it
+// dont change it unless your branch name is not "main" (/edit/BRANCH_NAME/src/pages/)
+
 export const GET_STARTED_LINK = '/getting-started'
 // used for landing page and nav link (just type it without language path)
 
@@ -17,6 +21,7 @@ export type Frontmatter = {
   title: string
   description: string
   layout: string
+  file: string
 }
 
 type SidebarSchema = {
