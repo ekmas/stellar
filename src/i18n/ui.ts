979b0f798eeb30ce83838702d18cec7b61a7f048
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-11 03:07:50 +0100

    added more ui translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,6 +15,21 @@ export type LandingPageObj = {
   githubBtnText: string
 }
 
+export type NavObj = {
+  documentation: string
+}
+
+export type OnThisPage = {
+  onThisPage: string
+  scrollToTop: string
+}
+
+export type Misc = {
+  editThisPage: string
+  previous: string
+  next: string
+}
+
 export const LANDING_PAGE: LanguageObject<LandingPageObj> = {
   en: {
     description: 'Modern type-safe documentation theme for Astro.',
@@ -27,3 +42,36 @@ export const LANDING_PAGE: LanguageObject<LandingPageObj> = {
     githubBtnText: 'Izvorni kod',
   },
 } as const
+
+export const NAV: LanguageObject<NavObj> = {
+  en: {
+    documentation: 'Docs',
+  },
+  sr: {
+    documentation: 'Dokumentacija',
+  },
+} as const
+
+export const ON_THIS_PAGE: LanguageObject<OnThisPage> = {
+  en: {
+    onThisPage: 'On this page',
+    scrollToTop: 'Scroll to top',
+  },
+  sr: {
+    onThisPage: 'Na ovoj stranici',
+    scrollToTop: 'Skroluj do vrha',
+  },
+}
+
+export const MISC: LanguageObject<Misc> = {
+  en: {
+    editThisPage: 'Edit this page',
+    next: 'Next',
+    previous: 'Previous',
+  },
+  sr: {
+    editThisPage: 'Uredi ovu stranicu',
+    next: 'Sledeće',
+    previous: 'Prethodno',
+  },
+}
