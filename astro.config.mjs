1a11b31ee815cb7c270f3dada62bee8ef5942cdd
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-12 04:41:41 +0100

    installed react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
-import mdx from '@astrojs/mdx'
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://temp.com',
-  integrations: [tailwind(), mdx()],
-})
+  integrations: [tailwind(), mdx(), react()]
+});
