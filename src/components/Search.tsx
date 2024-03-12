69b6cfb7abe809176ccdf605a59b474ae6b7144c
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-12 07:30:15 +0100

    added search components

---
+++ src/components/Search.tsx
@@ -0,0 +1,101 @@
+import { useState, useRef, useEffect, type ChangeEvent } from 'react'
+import Fuse, { type FuseResult } from 'fuse.js'
+import clsx from 'clsx'
+import { type MarkdownRecord } from '@/types'
+import SearchResults from '@/components/SearchResults'
+
+export default function Search({ docs }: { docs: MarkdownRecord[] }) {
+  const [isOverlayActive, setIsOverlayActive] = useState(false)
+  const [matchedItems, setMatchedItems] = useState<MarkdownRecord[]>([])
+  const [message, setMessage] = useState<string | null>('')
+
+  useEffect(() => {
+    console.log(matchedItems)
+  }, [matchedItems])
+
+  const overlayRef = useRef(null)
+
+  const options = {
+    keys: ['title', 'description'],
+    isCaseSensitive: false,
+  }
+
+  const fuse = new Fuse(docs, options)
+
+  const closeOverlay = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === overlayRef.current) {
+      setIsOverlayActive(false)
+    }
+  }
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value.trim()
+
+    if (inputValue.length === 0) {
+      setMatchedItems([])
+      if (message) setMessage(null)
+    } else if (inputValue.length > 0 && inputValue.length < 3) {
+      setMessage('Keep typing...')
+    } else {
+      const searchResult: FuseResult<MarkdownRecord>[] = fuse.search(inputValue)
+      const items: MarkdownRecord[] = searchResult.map((result) => result.item)
+      setMatchedItems(items)
+
+      if (items.length === 0) {
+        setMessage('No results')
+      } else {
+        setMessage(null)
+      }
+    }
+  }
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setIsOverlayActive(true)
+        }}
+        className="ml-10 flex items-center gap-4 rounded-md px-6 py-2 outline outline-1 outline-black/20 transition-[background-color] hover:bg-black/20 dark:outline-white/20 dark:hover:bg-white/20"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+            className="stroke-black dark:stroke-white"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+
+        <p>Search</p>
+      </button>
+
+      <div
+        ref={overlayRef}
+        onClick={closeOverlay}
+        className={clsx(
+          'fixed left-0 top-0 z-40 h-[100dvh] w-full py-20 transition-all duration-300',
+          isOverlayActive
+            ? 'visible bg-black/20 opacity-100 dark:bg-white/20'
+            : 'invisible bg-transparent opacity-0',
+        )}
+      >
+        <div className="mx-auto flex h-full w-[700px] flex-col rounded-xl border border-black/50 bg-neutral-100 p-10 dark:border-white/50 dark:bg-neutral-950">
+          <input
+            onChange={handleInputChange}
+            className="w-full rounded-md bg-transparent p-[10px] px-5 text-xl outline outline-1 outline-black/20 transition-[outline] focus:outline-black/50 dark:outline-white/20 dark:focus:outline-white/50"
+            placeholder="Search"
+            type="text"
+          />
+
+          <SearchResults message={message} matchedItems={matchedItems} />
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,34 @@
+import type { MarkdownRecord } from '@/types'
+
+export default function SearchResults({
+  message,
+  matchedItems,
+}: {
+  message: string | null
+  matchedItems: MarkdownRecord[]
+}) {
+  return (
+    <div className="mt-5 h-[calc(100%-20px-40px)] w-full shrink rounded-md border border-black/20 p-[10px] dark:border-white/20">
+      {message ? (
+        <div className="flex h-full w-full items-center justify-center">
+          <p className="text-lg">{message}</p>
+        </div>
+      ) : (
+        <div className="search-list flex h-full w-full flex-col gap-[10px] overflow-y-auto p-[5px]">
+          {matchedItems.map((item) => {
+            return (
+              <a
+                href={location.origin + item.url}
+                className="w-full rounded-md p-[10px] transition-all hover:bg-black/10 dark:hover:bg-white/10"
+              >
+                <h4 className="text-xl">{item.title}</h4>
+
+                <p className="mt-2 opacity-80">{item.description}</p>
+              </a>
+            )
+          })}
+        </div>
+      )}
+    </div>
+  )
+}
