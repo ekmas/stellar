7c958f19b0eb060e5eeb69d9243fc3a2dd268f88
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-14 19:48:17 +0100

    installed astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import expressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://temp.com',
-  integrations: [tailwind(), mdx(), react()]
-});
+  integrations: [
+    tailwind(),
+    expressiveCode({
+      themes: ['github-dark', 'github-light'],
+      styleOverrides: {
+        frames: {
+          editorActiveTabIndicatorTopColor: 'transparent',
+          editorActiveTabBorderColor: '#80808080',
+          editorTabBarBorderBottomColor: '#80808080',
+        },
+        uiFontFamily: 'inherit',
+        borderColor: '#80808080',
+      },
+    }),
+    mdx(),
+    react(),
+  ],
+})
