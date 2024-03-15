6ecd9169f8905db5e6d5d1cdf1070c84d6d12d8c
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-15 20:46:35 +0100

    added some screens

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,19 @@ import typography from '@tailwindcss/typography'
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        w1600: { max: '1600px' },
+        w1400: { max: '1400px' },
+        w1200: { max: '1200px' },
+        w1100: { max: '1100px' },
+        w900: { max: '900px' },
+        w800: { max: '800px' },
+        w600: { max: '600px' },
+        w500: { max: '500px' },
+        w400: { max: '400px' },
+      },
+    },
   },
   plugins: [typography],
   darkMode: 'class',
