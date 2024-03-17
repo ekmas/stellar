795d330e2527955e0a1efb5519f563e0eb917161
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-17 21:51:03 +0100

    added colors and border radius to tw.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import typography from '@tailwindcss/typography'
+import colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -15,6 +16,28 @@ export default {
         w600: { max: '600px' },
         w500: { max: '500px' },
         w400: { max: '400px' },
+      },
+      colors: {
+        lightModeText: '#000',
+        darkModeText: '#fff',
+
+        lightModeBg: colors.neutral[100],
+        darkModeBg: colors.neutral[950],
+
+        lightModeIcon: '#000',
+        darkModeIcon: '#fff',
+
+        lightModeBgHover: colors.neutral[300],
+        darkModeBgHover: colors.neutral[700],
+      },
+      backgroundImage: {
+        lightModeGradient:
+          'radial-gradient(ellipse 80% 80% at 50% -20%,rgba(120,119,198,0.5),rgba(0,0,0,0))',
+        darkModeGradient:
+          'radial-gradient(ellipse 80% 80% at 50% -20%,rgba(120,119,198,0.3),rgba(255,255,255,0))',
+      },
+      borderRadius: {
+        base: '10px', // dont make this value greater than 30px because it can make some elements look weird e.g. search element and search results
       },
     },
   },
