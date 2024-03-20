17a0334ddca2d333582809e946eaaee51a0faf43
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-20 04:31:15 +0100

    updated config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import { type LanguageKeys } from '@/i18n/ui'
+import { type ThemeObjectOrShikiThemeName } from 'astro-expressive-code'
 
 export const PROJECT_NAME = 'Stellar'
 // used for landing page heading and nav home link
@@ -22,8 +23,21 @@ export const GITHUB_EDIT_URL = GITHUB_REPO_URL + '/edit/main/src/pages/'
 // used for edit url link at the bottom of each page in docs so users can edit it
 // dont change it unless your branch name is not "main" (/edit/BRANCH_NAME/src/pages/)
 
-export const GET_STARTED_LINK = '/getting-started'
+export const GET_STARTED_LINK = '/introduction'
 // used for landing page and nav link (just type it without language path)
 
 export const SINGLE_LANGUAGE = false
 // set this to true if you want to use only one language
+
+export const OG_IMAGE = 'og.png'
+// enter name of the og image file you put inside public folder
+// image should be 1200 X 630 pixels
+
+export const LIGHT_MODE_CODE_BLOCK_THEME: ThemeObjectOrShikiThemeName =
+  'github-light'
+
+export const DARK_MODE_CODE_BLOCK_THEME: ThemeObjectOrShikiThemeName =
+  'github-dark'
+
+// list of all themes you can use: https://expressive-code.com/guides/themes/#available-themes
+// if u change any of those code block theme values make sure to change them as well in astro.config.mjs inside expressiveCode in themes array
